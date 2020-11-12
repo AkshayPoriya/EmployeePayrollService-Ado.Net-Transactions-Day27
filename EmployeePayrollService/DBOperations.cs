5065daaab4329027aa95a093afa5f6a8b2bba2f7
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-13 02:50:32 +0530

    [Akshay] Add employee data, update entries in all tables

--- EmployeePayrollService/DBOperations.cs
+++ EmployeePayrollService/DBOperations.cs
@@ -8,10 +8,73 @@ namespace EmployeePayrollService
 {
     using System;
     using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
     using System.Text;
 
-    class DBOperations
+    public class DBOperations
     {
+        public static void AddEmployee(Employee employee)
+        {
+            SqlConnection sqlConnection = DBConnection.GetConnection();
+            try
+            {
+                using (sqlConnection)
+                {
+                    sqlConnection.Open();
+                    SqlCommand sqlCommand = new SqlCommand("dbo.spAddNewEmployee", sqlConnection);
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@employee_id", employee.employeeID);
+                    sqlCommand.Parameters.AddWithValue("@employee_name", employee.employeeName);
+                    sqlCommand.Parameters.AddWithValue("@gender", employee.gender);
+                    sqlCommand.Parameters.AddWithValue("@address", employee.address);
+                    sqlCommand.Parameters.AddWithValue("@start_date", employee.startDate);
+                    sqlCommand.Parameters.AddWithValue("@department_id", employee.departmentID);
+                    sqlCommand.Parameters.AddWithValue("@department_name", employee.departmentName);
+                    sqlCommand.Parameters.AddWithValue("@basic_pay", employee.BasicPay);
+                    sqlCommand.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                if (sqlConnection.State == ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
 
+        public static double GetSalary(string name)
+        {
+            SqlConnection sqlConnection = DBConnection.GetConnection();
+            try
+            {
+                using (sqlConnection)
+                {
+                    sqlConnection.Open();
+                    string query = @"select p.basic_pay from dbo.payroll p 
+                                    where p.emp_id = (select e.id from employee e where name=@name)";
+                    SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@name", name);
+                    double basicPay = (double)sqlCommand.ExecuteScalar();
+                    return basicPay;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            finally
+            {
+                if (sqlConnection.State == ConnectionState.Open)
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
     }
 }
---
+++ EmployeePayrollService/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    public class Employee
+    {
+        public int employeeID { get; set; }
+        public string employeeName { get; set; }
+        public string gender { get; set; }
+        public string address { get; set; }
+        public DateTime startDate { get; set; }
+        public int departmentID { get; set; }
+        public string departmentName { get; set; }
+        public double BasicPay { get; set; }
+
+        public Employee()
+        {
+
+        }
+    }
+}
